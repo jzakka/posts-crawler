15bab770182b5856e6cd0c6840ede346e58766fe
Jzakka <mouse4786@gmail.com>
2023-09-13 19:51:54 +0900

    feat:스레드 자동 재시작

--- crawler.py
+++ crawler.py
@@ -5,6 +5,16 @@ DOMAIN="https://gall.dcinside.com"
 BASE_URL=DOMAIN +"/board/lists/"
 headers={'User-Agent':'Chrome/'}
 
+def crawl_gallery(gallery_id):
+    try:
+        crawl.gallery_crawl(DOMAIN, BASE_URL, gallery_id.strip(), headers, 0)
+    except Exception as e:
+        # 스레드에서 예외 발생 시 예외 처리
+        print(f"스레드에서 예외 발생: {e}")
+        # 필요한 로깅 또는 기타 작업 수행
+        # 스레드를 다시 시작하려면 새로운 스레드를 생성
+        threading.Thread(target=crawl_gallery, args=(gallery_id,)).start()
+
 with open("gallery_list.txt", "r") as file:
     for gallery_id in file:
-        threading.Thread(target=crawl.gallery_crawl, args=(DOMAIN, BASE_URL, gallery_id.strip(), headers, 0)).start()
+        threading.Thread(target=crawl_gallery, args=(gallery_id,)).start()
